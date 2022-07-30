3495e1255642dd757f2872021a3fc31110712833
Carla <carla.valle@engenharia.ufjf.br>
2022-07-30 15:58:22 -0300

    feat: add chat room entry

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
 let messages = [];
 
+function login() {
+    document.querySelector(".homeScreen").classList.add("hidden");
+    document.querySelector(".mainScreen").classList.remove("hidden");
+    const name = document.querySelector(".username").value;
+    const newUser = {name: name};
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", newUser);
+    promise.then(loadMessages);
+    promise.catch(handleError);
+}
+
 loadMessages();
+
+function handleError(error) {
+    console.log(error.response);
+    if (error.response.status === 400) {
+        alert("Esse usuario já esta cadastrado.")
+    }
+}
 
 function loadMessages() {
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -16,28 +33,27 @@ function renderMessages() {
     const ulMessages = document.querySelector(".container");
     ulMessages.innerHTML = "";
     for (let i = 0; i < messages.length; i++) {
-    console.log(messages);
-    if (messages[i].type === "status") {
-        ulMessages.innerHTML += `
-            <div class="message status">
+        if (messages[i].type === "status") {
+            ulMessages.innerHTML += `
+                <div class="message status">
+                    <div class="hour">(${messages[i].time})</div>
+                    <div class="messageContent">${messages[i].from} ${messages[i].text}</div>
+                </div>   
+                `
+        } else if (messages[i].type === "message") {
+            ulMessages.innerHTML += `
+            <div class="message normal">
                 <div class="hour">(${messages[i].time})</div>
-                <div class="messageContent">${messages[i].from} ${messages[i].text}</div>
-            </div>   
+                <div class="messageContent">${messages[i].from} para ${messages[i].to}: ${messages[i].text}</div>
+            </div>
             `
-    } else if (messages[i].type === "message") {
-        ulMessages.innerHTML += `
-        <div class="message normal">
-            <div class="hour">(${messages[i].time})</div>
-            <div class="messageContent">${messages[i].from} para ${messages[i].to}: ${messages[i].text}</div>
-        </div>
-        `
-    } else {
-        ulMessages.innerHTML += `
-        <div class="message reserved">
-            <div class="hour">(${messages[i].time})</div>
-            <div class="messageContent">${messages[i].from} para ${messages[i].to}: ${messages[i].text}</div>
-        </div>
-        `
-    }
+        } else {
+            ulMessages.innerHTML += `
+            <div class="message reserved">
+                <div class="hour">(${messages[i].time})</div>
+                <div class="messageContent">${messages[i].from} para ${messages[i].to}: ${messages[i].text}</div>
+            </div>
+            `
+        }
     }
 }
